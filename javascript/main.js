b8719adac59c52b8dca9f9a9a372cbedea26095a
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-05-02 22:50:22 -0400

    setup round start and ui functions

--- javascript/main.js
+++ javascript/main.js
@@ -10,21 +10,66 @@
     var player;
     var state;
     var dealer;
+    var pot;
 
     function init(){
         createPlayers();
+        setDealer();
+        startRound();
     }
 
     function createPlayers(){
         opponent = {
             "money": starting_money,
             "cards": [],
+            "wins": 0,
+            "loses": 0,
         };
 
         player = {
             "money": starting_money,
             "cards": [],
+            "wins": 0,
+            "loses": 0,
         };
+    }
+
+    function setDealer(){
+        dealer = (dealer === undefined) ? (Math.round(Math.random())) : (dealer === 0) ? 1 : 0;
+        return;
+    }
+
+    function startRound(){
+        pot = 0;
+        state = 1;
+        updateUI();
+        checkState();
+    }
+
+    function checkState(){
+        switch(state){
+            case(1):
+                if(dealer === 0){
+                    playerFirstRound();
+                }else{
+                    opponentFirstRound();
+                }
+                break;
+
+
+            default:
+                showError("unknown state:" + state);
+        }
+    }
+
+    function updateUI() {
+        $("#player_money").html("$" + numberWithCommas(player.money));
+        $("#opp_money").html("$" + numberWithCommas(opponent.money));
+        $("#opp_won").html(opponent.wins);
+        $("#opp_lost").html(opponent.loses);
+        $("#player_won").html(player.wins);
+        $("#player_lost").html(player.loses);
+        $("#money_pot").html("$" + numberWithCommas(pot));
     }
 
     function saveGame(){
@@ -33,6 +78,10 @@
 
     function loadGame(){
         //TODO:: Load saved game and inject saved state onto gameboard.
+    }
+
+    function numberWithCommas(number){
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
     $(document).ready(init);
